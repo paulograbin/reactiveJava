2e1d8cafc69d8f451404c77e38367d2b1c908c78
Grabin, Paulo <paulo.grabin@sap.com>
2019-06-22 15:16:09 -0300

    Revert "Removing duplicated package name"
    This reverts commit 511a56a748c00207c819673cd7eb13014e00cab9.

---
+++ src/main/java/com/greglturnquist/learningspringboot/learningspringboot/webdriver/FirefoxDriverFactory.java
@@ -0,0 +1,29 @@
+package com.greglturnquist.learningspringboot.learningspringboot.webdriver;
+
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.ObjectFactory;
+
+
+public class FirefoxDriverFactory implements ObjectFactory<FirefoxDriver> {
+
+    private WebDriverConfigurationProperties properties;
+
+    public FirefoxDriverFactory(WebDriverConfigurationProperties properties) {
+        this.properties = properties;
+    }
+
+    @Override
+    public FirefoxDriver getObject() throws BeansException {
+        if (properties.getFirefox().isEnabled()) {
+            try {
+                return new FirefoxDriver();
+            } catch (WebDriverException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return null;
+    }
+}
