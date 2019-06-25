ead28cfc83852db953112f5067ca61613a64375f
Grabin, Paulo <paulo.grabin@sap.com>
2019-06-25 11:28:56 -0300

    Add profile to comment simulator

--- src/main/java/com/greglturnquist/learningspringboot/CommentSimulator.java
+++ src/main/java/com/greglturnquist/learningspringboot/CommentSimulator.java
@@ -4,6 +4,7 @@ import com.greglturnquist.learningspringboot.comments.Comment;
 import com.greglturnquist.learningspringboot.comments.CommentController;
 import com.greglturnquist.learningspringboot.images.ImageRepository;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
@@ -12,6 +13,7 @@ import reactor.core.publisher.Mono;
 import java.time.Duration;
 import java.util.concurrent.atomic.AtomicInteger;
 
+@Profile("simulator")
 @Component
 public class CommentSimulator {
 
@@ -30,7 +32,7 @@ public class CommentSimulator {
     @EventListener
     public void onApplicationReadyEvent(ApplicationReadyEvent event) {
         Flux
-                .interval(Duration.ofMillis(10))
+                .interval(Duration.ofMillis(1000))
                 .flatMap(tick -> imageRepository.findAll())
                 .map(image -> {
                     Comment comment = new Comment();
