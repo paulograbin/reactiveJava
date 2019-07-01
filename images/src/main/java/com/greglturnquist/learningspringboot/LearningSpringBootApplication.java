b52fc36b4706e9066bd4f2af63926683fef1c24c
Grabin, Paulo <paulo.grabin@sap.com>
2019-06-30 22:10:59 -0300

    Replace main class annotation from SpringBootAplication to SpringCloudApplication

--- images/src/main/java/com/greglturnquist/learningspringboot/LearningSpringBootApplication.java
+++ images/src/main/java/com/greglturnquist/learningspringboot/LearningSpringBootApplication.java
@@ -1,26 +1,17 @@
 package com.greglturnquist.learningspringboot;
 
-import org.hibernate.validator.messageinterpolation.ParameterMessageInterpolator;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.web.filter.reactive.HiddenHttpMethodFilter;
 
-@SpringBootApplication
-@EnableReactiveMongoRepositories
+
+@SpringCloudApplication
 public class LearningSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearningSpringBootApplication.class, args);
 	}
-
-	@Bean
-	ParameterMessageInterpolator parameterMessageInterpolator() {
-		return new ParameterMessageInterpolator();
-	}
-
-
 
 	@Bean
 	HiddenHttpMethodFilter hiddenHttpMethodFilter() {
