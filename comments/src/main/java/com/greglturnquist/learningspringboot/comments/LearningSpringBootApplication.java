cb0c03e18e2a7aca698e8a74edb0d4e6aac9b253
Grabin, Paulo <paulo.grabin@sap.com>
2019-07-27 19:10:04 -0300

    Remove commented code

--- comments/src/main/java/com/greglturnquist/learningspringboot/comments/LearningSpringBootApplication.java
+++ comments/src/main/java/com/greglturnquist/learningspringboot/comments/LearningSpringBootApplication.java
@@ -2,6 +2,8 @@ package com.greglturnquist.learningspringboot.comments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.reactive.HiddenHttpMethodFilter;
 
 
 @SpringBootApplication
@@ -19,10 +21,5 @@ public class LearningSpringBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(LearningSpringBootApplication.class, args);
     }
-
-//	@Bean
-//	ParameterMessageInterpolator parameterMessageInterpolator() {
-//		return new ParameterMessageInterpolator();
-//	}
 
 }
